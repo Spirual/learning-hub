f337fae6964ab140eb17780214bc5e57c7da9928
Spirual <spiuaal@gmail.com>
2024-02-29 13:34:52 +0300

    Настроена админ зона проекта.

--- backens/products/admin.py
+++ backens/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course, CourseAccess, Lesson, Cohort, CohortMembership
 
-# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(CourseAccess)
+admin.site.register(Lesson)
+admin.site.register(Cohort)
+admin.site.register(CohortMembership)
