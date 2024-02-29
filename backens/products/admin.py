71b68a7d2ed6b465ac29f391adc5b6771bafe14c
Spirual <spiuaal@gmail.com>
2024-03-01 00:49:45 +0300

    Настроена админ-зона проекта.

--- backens/products/admin.py
+++ backens/products/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-from .models import Course, CourseAccess, Lesson, Cohort, CohortMembership
+from .models import Course, Lesson, Cohort, CohortMembership
 
 
-admin.site.register(Course)
-admin.site.register(CourseAccess)
-admin.site.register(Lesson)
-admin.site.register(Cohort)
-admin.site.register(CohortMembership)
+class CohortAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course_name')
+    list_select_related = ('course',)
+
+    def course_name(self, obj):
+        return obj.course.title
+    course_name.short_description = 'Название курса'
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'start_date_time', 'max_users')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course_name')
+
+    def course_name(self, obj):
+        return obj.courses.title
+    course_name.short_description = 'Название курса'
+
+
+class CohortMembershipAdmin(admin.ModelAdmin):
+    list_display = ('cohort', 'student')
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Cohort, CohortAdmin)
+admin.site.register(CohortMembership, CohortMembershipAdmin)
