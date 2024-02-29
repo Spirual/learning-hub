29d61014315fb7dd95008919cdb626318e9f01fb
Spirual <spiuaal@gmail.com>
2024-03-01 00:26:27 +0300

    Реализована ручка покупки курса.

--- backens/products/views.py
+++ backens/products/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -28,3 +29,33 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
         return Response(serializer.data)
 
+    @action(detail=True, methods=['get'], permission_classes=[IsAuthenticated])
+    def buy(self, request, slug):
+        course = get_object_or_404(Course, slug=slug)
+
+        user = request.user
+        cohorts = course.cohorts.prefetch_related('users').all()
+
+        if not cohorts.exists():
+            return Response({'message': 'Для данного курса еще нет когорт'}, status=status.HTTP_403_FORBIDDEN)
+
+        if course.start_date_time < timezone.now():
+            return Response(
+                {'message': 'Вы опоздали на данный курс, он уже начался'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        if CohortMembership.objects.filter(student=user, cohort__course=course).exists():
+            return Response({'error': 'Вы уже приобрели данный курс'}, status=status.HTTP_403_FORBIDDEN)
+
+        for cohort in cohorts:
+            if cohort.users.count() <= course.max_users:
+                CohortMembership.objects.create(cohort=cohort, student=user)
+                message = (f'Поздравляем! Вы успешно приобрели курс: "{course.title}"'
+                           f' и зачислены в когорту: "{cohort.title}"')
+                return Response({'message': message}, status=status.HTTP_200_OK)
+
+        return Response(
+            {'message': 'Извините, все когорты для данного курса заполнены. Дождитесь следующего набора'},
+            status=status.HTTP_403_FORBIDDEN
+        )
